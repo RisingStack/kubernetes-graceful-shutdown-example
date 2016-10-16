8c328978fc9626fd931d5c6c3142a62534703efe
Peter Marton <peter@risingstack.com>
2016-10-16 21:17:49 +0200

    docs(readme): add explanation

--- README.md
+++ README.md
@@ -1,3 +1,73 @@
 # kubernetes-graceful-stop-example
 
-// TODO
+Example repository to give a help with Kubernetes graceful start and shutdown.
+
+## What it does?
+
+1. pod receives *SIGTERM* signal because Kubernetes wants to stop the it because of deploy, scale etc.
+2. App *(pod)* starts to return `500` for `GET /health` to let `readinessProbe` *(Service)* know that it's not ready to receive more requests.
+3. Kubernetes `readinessProbe` checks `GET /health` and after *(failureThreshold * periodSecond)* it stops redirecting traffic after it *(because it continuously returns 500)*
+4. App waits *(failureThreshold * periodSecond)* before starts shutdown, to wait for `readinessProbe` fail
+5. App starts graceful shutdown
+6. App first close server with live working DB connections
+7. App close databases after the server is closed
+8. App exists process
+9. Kubernetes force kill application after 30s *(SIGKILL)* if it's still running (in an optimal case it doesn't happen)
+
+In our case Kubernetes `livenessProbe` won't kill the app before graceful shutdown because needs to wait *(failureThreshold * periodSecond)* to do it, so `livenessProve` threshold should be larger than `readinessProbe` threshold *(graceful stop happens around 10s, force kill would happen 30s after SIGTERM)*
+
+## Benchmark
+
+### Test case
+
+```
+$ ab -n 100000 -c 20
+```
+
+Plus changing an environment variable in the `Deployment` to re-deploy all pods during the `ab` benchmarking.
+
+### AB output
+
+```
+Document Path:          /
+Document Length:        3 bytes
+
+Concurrency Level:      20
+Time taken for tests:   172.476 seconds
+Complete requests:      100000
+Failed requests:        0
+Total transferred:      7800000 bytes
+HTML transferred:       300000 bytes
+Requests per second:    579.79 [#/sec] (mean)
+Time per request:       34.495 [ms] (mean)
+Time per request:       1.725 [ms] (mean, across all concurrent requests)
+Transfer rate:          44.16 [Kbytes/sec] received
+```
+
+### Application log output
+
+```
+Got SIGTERM. Graceful shutdown start 2016-10-16T18:54:59.208Z
+Request after sigterm: / 2016-10-16T18:54:59.217Z
+Request after sigterm: / 2016-10-16T18:54:59.261Z
+...
+Request after sigterm: / 2016-10-16T18:55:00.064Z
+Request after sigterm: /health?type=readiness 2016-10-16T18:55:00.820Z
+HEALTH: NOT OK
+Request after sigterm: /health?type=readiness 2016-10-16T18:55:02.784Z
+HEALTH: NOT OK
+Request after sigterm: /health?type=liveness 2016-10-16T18:55:04.781Z
+HEALTH: NOT OK
+Request after sigterm: /health?type=readiness 2016-10-16T18:55:04.800Z
+HEALTH: NOT OK
+Server is shutting down... 2016-10-16T18:55:05.210Z
+Successful graceful shutdown 2016-10-16T18:55:05.212Z
+```
+
+## Known issues
+
+### keep-alive
+
+Kubernetes doesn't handover keep-alive connections.
+Request from agents with keep-alive header will be still routed to the pod.
+It's tricked me first when I benchmarked with [autocannon](https://github.com/mcollina/autocannon) or `Google Chrome`.
--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ const enableDestroy = require('server-destroy')
 const db = require('./db')
 const server = require('./server')
 const port = process.env.PORT || 3000
-const DEBUG_DELAY = 2000
-let READINESS_PROBE_DELAY = 2 * 2 * 1000     		// failureThreshold: 2, periodSeconds: 2 (4s)
-READINESS_PROBE_DELAY += DEBUG_DELAY            // just for demonstrate that really doesn't receive requests after 4s
+const DEBUG_DELAY = 2000                          // just for demonstrate that it really doesn't receive requests after 4s
+const READINESS_PROBE_DELAY = 2 * 2 * 1000     		// failureThreshold: 2, periodSeconds: 2 (4s)
 
+// Keep-alive connections doesn't let the server to close in time
+// Destory extension helps to force close connections
+// Because we wait READINESS_PROBE_DELAY, we expect that all requests are fulfilled
+// https://en.wikipedia.org/wiki/HTTP_persistent_connection
 enableDestroy(server)
 
 const serverListen = promisify(server.listen, server)
 const serverDestroy = promisify(server.destroy, server)
 
 // craceful start
-db.connect()                                      // open db connection first
+db.connect()                                      // open DB connection first
   .then(() => serverListen(port))                 // listen after succss db connection
   .then(() => {
     console.info(`App is listening on port: ${port}`)
@@ -25,14 +28,14 @@ db.connect()                                      // open db connection first
     console.error(err)
   })
 
-// gcraceful stop
+// Graceful stop
 function greacefulStop () {
   console.info('Server is shutting down...', new Date().toISOString())
 
-  serverDestroy()   										            // close server first (ongoing requests)
-    .then(() => db.close())  		                  // close db after it's not needed by server / worker task
+  serverDestroy()   										          // close server first (ongoing requests)
+    .then(() => db.close())  		                  // close DB after it's not needed by server / worker task
     .then(() => {
-      console.info('Succesfull graceful shutdown', new Date().toISOString())
+      console.info('Successful graceful shutdown', new Date().toISOString())
       process.exit(0)									            // exit with ok code
     })
     .catch((err) => {
@@ -41,13 +44,13 @@ function greacefulStop () {
     })
 }
 
-// support gracefull shutdown
-// do not accept more request
+// Support graceful shutdown
+// do not accept more request and release resources
 process.on('SIGTERM', function onSigterm () {
   console.info('Got SIGTERM. Graceful shutdown start', new Date().toISOString())
 
   // Wait a little bit to give enough time for Kubernetes readiness probe to fail (we don't want more traffic)
   // Don't worry livenessProbe won't kill it until (failureThreshold: 3) => 30s
   // http://www.bite-code.com/2015/07/27/implementing-graceful-shutdown-for-docker-containers-in-go-part-2/
-  setTimeout(greacefulStop, READINESS_PROBE_DELAY)
+  setTimeout(greacefulStop, READINESS_PROBE_DELAY + DEBUG_DELAY)
 })
